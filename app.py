79ec8f9119fd08e1c8d36a2c68dba424f37bd4de
ratspin <6210110013@psu.ac.th>
2022-01-16 17:57:39 +0700

    test commit

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "Hello My First Web Application 2"
+    return "Hello My First Web Application"
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port='5000',debug=True)
-    
